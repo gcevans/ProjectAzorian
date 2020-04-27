6a5123b0aa5b386b80a2a046066282ff91395a05
G Carl Evans <gcevans@gmail.com>
2020-04-27 10:34:44 -0500

    pre lecture cleanup

--- main.cpp
+++ main.cpp
@@ -20,6 +20,22 @@ typedef Edge<Vertex<string>, int> e_type;
 typedef shared_ptr<Edge<Vertex<string>, int>> ep_type;
 typedef Graph<v_type, e_type> g_type;
 
+vector<vp_type> make_vertices(const vector<string> &names) {
+    vector<vp_type> vertices;
+    for(string name : names) {
+        vertices.push_back(vp_type(new v_type(name) ) );
+    }
+    return vertices;
+}
+
+vector<ep_type> make_edges(const vector<vp_type> &v, const vector<pair<int,int>> &connections) {
+    vector<ep_type> edges;    
+    for(auto edge : connections) {
+        edges.push_back(ep_type(new e_type(v[edge.first],v[edge.second],1)));
+    }
+    return edges;
+}
+
 int main() {
 
     vector<string> names = {"A","B","C","D","E","F","G","H"};
@@ -38,16 +54,8 @@ int main() {
     connections.push_back(make_pair(5,6));
     connections.push_back(make_pair(6,7));
 
-    vector<vp_type> vertices;
-    vector<ep_type> edges;
-
-    for(string name : names) {
-        vertices.push_back(vp_type(new v_type(name) ) );
-    }
-
-    for(auto edge : connections) {
-        edges.push_back(ep_type(new e_type(vertices[edge.first],vertices[edge.second],1)));
-    }
+    vector<vp_type> vertices = make_vertices(names);
+    vector<ep_type> edges = make_edges(vertices, connections);
 
     g_type G(vertices, edges);
 
